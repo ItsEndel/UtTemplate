52697dbd48fe161939159b805a3d584365f67ad9
ItsEndel <74142598+ItsEndel@users.noreply.github.com>
2022-04-08 19:54:04 +0800

    object class

---
+++ Classes/Object/Object.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class Object : Node2D
+{
+    // 主节点
+    protected Trigger trigger;
+
+    // GD方法
+    public override void _Ready()
+    {
+        // 获取主节点
+        trigger = GetNode<Trigger>("Trigger");
+
+        // 连接目标触发方法
+        trigger.Connect("area_entered", this, "triggered");
+    }
+
+    // 目标触发方法
+    public virtual void triggered(Area2D area) {
+
+    }
+}
