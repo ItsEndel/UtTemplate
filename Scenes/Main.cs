2793ef87e9d3cb33545fae040d81f1089d58896a
ItsEndel <74142598+ItsEndel@users.noreply.github.com>
2022-04-12 15:28:46 +0800

    moved test scripts to test scene

--- Scenes/Main.cs
+++ Scenes/Main.cs
@@ -20,25 +20,6 @@ public class Main : Node
         GD.Randomize(); // 初始化随机数种子
         OS.SetWindowTitle("UNDERTALE"); // 修改窗口名字
         this.AddChild(_launchScene.Instance()); // 加载启动场景
-
-        // 测试
-        string text = "[wait=10]";
-        text = text + "测 试 测 试 TesT tESt";
-        text = text + "\n换行测试";
-        text = text + "\n延迟[wait=1]测试";
-        text = text + "\n[size=32][line_space=36][full_char_space=36][half_char_space=20]尺寸测试";
-        text = text + "\n[color=(1, 0, 0, 1)]C[color=(0, 1, 0, 1)]O[color=(0, 1, 0, 1)]L[color=(1, 1, 0, 1)]O[color=(0, 1, 1, 1)]R[color=(1, 1, 1, 1)]";
-        text = text + "\n[outline_size=2][outline_color=(1, 0, 0, 1)]OUTLINE TEST 轮廓线测试[outline_size=0]";
-        text = text + "\n[shake=()]default shake[/shake=]";
-        text = text + "\n[shake=(frequency=1000)]faster shake[/shake=]";
-        text = text + "\n[shake=(amplitude=5,max_amplitude=25)]bigger shake[/shake=]";
-        text = text + "\n[position=(200, 200)]POSITION TEST\n\n";
-        for (int i = 0; i < 100; i ++) {
-            Color color = Color.FromHsv(i / 100f, 1, 1);
-            text = text + "[color=(" + color.r + "," + color.g + "," + color.b + "," + color.a + ")]O";
-        }
-        TextDisplayer textDisplayer = new TextDisplayer(text);
-        AddChild(textDisplayer);
     }
 
     public override void _Process(float delta)
--- Scenes/Rooms/Test/test.cs
+++ Scenes/Rooms/Test/test.cs
@@ -9,7 +9,30 @@ public class test : Node2D
 
     public override void _Ready()
     {
+        // 测试
         Update();
+        
+        // 测试
+        string text = "[wait=5]";
+        text = text + "测 试 测 试 TesT tESt";
+        text = text + "\n换行测试";
+        text = text + "\n延迟[wait=1]测试";
+        text = text + "\n[size=24][line_space=27][full_char_space=27][half_char_space=15]尺寸测试";
+        text = text + "\n[color=(1, 0, 0, 1)]C[color=(0, 1, 0, 1)]O[color=(0, 1, 0, 1)]L[color=(1, 1, 0, 1)]O[color=(0, 1, 1, 1)]R[color=(1, 1, 1, 1)]";
+        text = text + "\n[outline_size=2][outline_color=(1, 0, 0, 1)]OUTLINE TEST 轮廓线测试[outline_size=0]";
+        text = text + "\n[shake=()]default shake[/shake=]";
+        text = text + "\n[shake=(frequency=1000)]faster shake[/shake=]";
+        text = text + "\n[shake=(amplitude=5,max_amplitude=25)]bigger shake[/shake=]";
+        text = text + "\n[position=(200, 200)]POSITION TEST\n";
+        text = text + "\n!THIS IS SKIP TEST\\!!";
+        text = text + "\n[theme=sans_dialog]预设测试 theme test[half_char_space=15]";
+        text = text + "\n[voice=Voice/voc_flowey.wav]";
+        for (int i = 0; i < 100; i ++) {
+            Color color = Color.FromHsv(i / 100f, 1, 1);
+            text = text + "[color=(" + color.r + "," + color.g + "," + color.b + "," + color.a + ")]O";
+        }
+        TextDisplayer textDisplayer = new TextDisplayer(text);
+        AddChild(textDisplayer);
     }
 
     public override void _Draw()
