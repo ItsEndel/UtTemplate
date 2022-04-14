6897112f624f34496f1ce6a0f24f83387747a6de
ItsEndel <74142598+ItsEndel@users.noreply.github.com>
2022-04-14 17:13:30 +0800

    bgm method & scene method

--- Scenes/Main.cs
+++ Scenes/Main.cs
@@ -1,25 +1,27 @@
 using Godot;
 using System;
-using System.Text.RegularExpressions;
 
 public class Main : Node
 {
     // 主节点
-    private Node _sounds;
+    private AudioStreamPlayer _bgm; // 背景音乐
+    private Node _sounds; // 音效
+    private Node _scene; // 当前场景
 
     // 启动场景
-    [Export] private PackedScene _launchScene = GD.Load<PackedScene>("res://Scenes/Rooms/Test/TestRoom.tscn");
+    [Export] private PackedScene _initScene = GD.Load<PackedScene>("res://Scenes/Init.tscn");
 
     // GD方法
     public override void _Ready()
     {
         // 获取主节点
+        _bgm = GetNode<AudioStreamPlayer>("BGM");
         _sounds = GetNode<Node>("Sounds");
         // 初始化
         Game.MainScene = this; // 设置主场景
         GD.Randomize(); // 初始化随机数种子
         OS.SetWindowTitle("UNDERTALE"); // 修改窗口名字
-        this.AddChild(_launchScene.Instance()); // 加载启动场景
+        SetScene(_initScene); // 加载启动场景
     }
 
     public override void _Process(float delta)
@@ -29,9 +31,27 @@ public class Main : Node
         }
     }
 
-    // 音效方法
-    public void PlaySound(AudioStream audio) {
+    // 场景方法
+    public void SetScene(PackedScene scene) {
+        if (_scene != null) {
+            _scene.QueueFree();
+        }
+
+        _scene = scene.Instance();
+        AddChild(_scene);
+    }
+
+    // 音频方法
+    public AudioStreamPlayer SetBGM(AudioStream audio) {
+        _bgm.Stream = audio;
+        _bgm.Play();
+
+        return _bgm;
+    }
+    public Sound PlaySound(AudioStream audio) {
         Sound newSound = new Sound(audio);
         _sounds.AddChild(newSound);
+
+        return newSound;
     }
 }
