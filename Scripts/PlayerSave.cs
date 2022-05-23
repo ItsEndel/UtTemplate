f125e1fb2019a4d687779cfddfd329f2caf2538a
ItsEndel <74142598+ItsEndel@users.noreply.github.com>
2022-05-23 13:25:34 +0800

    Player things

--- Scripts/PlayerSave.cs
+++ Scripts/PlayerSave.cs
@@ -6,4 +6,13 @@ public class PlayerSave {
 
     // 玩家名称
     public string Name;
+
+    // 玩家等级数据
+    public int Level = 1;
+    public int Exp = 0;
+
+    // 玩家血量数据
+    public int MaxHp = 20;
+    public int Hp = 20;
+    public int ExtraHp = 0;
 }
